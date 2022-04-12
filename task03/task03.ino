d2e4d83467fbf23a1e2a142f93fba5fb538f8656
Aleksandr Goriachkin <aleksandr.goriachkin@ihsmarkit.com>
2022-04-12 21:50:34 +0400

    Added tasks

--- task03/task03.ino
+++ task03/task03.ino
@@ -1,7 +1,68 @@
-void setup() {
+# define EN 8 // stepper motor enable , active low
+# define X_DIR 5 // X -axis stepper motor direction control
+# define Y_DIR 6 // y -axis stepper motor direction control
+# define Z_DIR 7 // z axis stepper motor direction control
+# define X_STP 2 // x -axis stepper control
+# define Y_STP 3 // y -axis stepper control
+# define Z_STP 4 // z -axis stepper control
+# define SPEED_INTERVAL 20
 
+struct Engine{
+  bool dir;
+  int stepperPin;
+  unsigned long previousMicros;
+  unsigned long interval; 
+  int state;
+};
+
+struct Engine engines[] = {
+  false, X_STP, 0, SPEED_INTERVAL, LOW,
+  false, Z_STP, 0, SPEED_INTERVAL, LOW
+};
+
+void setup () {
+  pinMode (X_DIR, OUTPUT); pinMode (X_STP, OUTPUT);
+  //pinMode (Y_DIR, OUTPUT); pinMode (Y_STP, OUTPUT);
+  pinMode (Z_DIR, OUTPUT); pinMode (Z_STP, OUTPUT);
+  pinMode (EN, OUTPUT);
+  digitalWrite (EN, LOW);
+
+//  Serial.begin(9600);
 }
 
-void loop() {
-
+// -----------------------------------------
+//
+// Сделать на таймерах!
+//
+// -----------------------------------------
+void loop () {
+  
+  unsigned long currentMicros = micros();
+//
+//  Serial.print(currentMicros);
+//  Serial.print('\n');
+//  
+  digitalWrite (engines[0].stepperPin, false);
+  digitalWrite (engines[1].stepperPin, false);
+    
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 100; j++)
+  {
+    delayMicroseconds (50);
+//      Serial.print("Engine [");
+//      Serial.print(i);
+//      Serial.print("], Micros of this Engine = ");
+//      Serial.print(engines[i].previousMicros);
+//      Serial.print('\n');
+//      Serial.print("Engine state = ");
+//      Serial.print(engines[i].state); 
+//      Serial.print('\n');
+//      
+      delayMicroseconds (20);
+      digitalWrite(engines[i].stepperPin, HIGH);
+      delayMicroseconds (20);
+      digitalWrite(engines[i].stepperPin, LOW);
+    }
+  }
 }
--- task04/task04.ino
+++ task04/task04.ino
@@ -1,7 +1,41 @@
-void setup() {
+# define ENABLE_PIN 8 // stepper motor enable , active low
+# define X_DIR 5 // X -axis stepper motor direction control
+# define Y_DIR 6 // y -axis stepper motor direction control
+# define Z_DIR 7 // z axis stepper motor direction control
+# define X_STP 2 // x -axis stepper control
+# define Y_STP 3 // y -axis stepper control
+# define Z_STP 4 // z -axis stepper control
+# define SPEED_INTERVAL 20
 
+#include <TaskManagerIO.h>
+
+bool isEnabledPinX = false;
+bool isEnabledPinZ = false;
+
+void ChangeStateX() {
+    digitalWrite(X_STP, isEnabledPinX);
+    isEnabledPinX = !isEnabledPinX;           
+}
+
+void ChangeStateZ() {
+    digitalWrite(Z_STP, isEnabledPinZ);
+    isEnabledPinZ = !isEnabledPinZ;           
+}
+
+void setup() {
+  pinMode (X_DIR, OUTPUT); 
+  pinMode (X_STP, OUTPUT);
+  pinMode (Z_DIR, OUTPUT); 
+  pinMode (Z_STP, OUTPUT);
+  
+  pinMode (ENABLE_PIN, OUTPUT);
+  
+  digitalWrite (ENABLE_PIN, LOW);
+  
+  taskManager.scheduleFixedRate(SPEED_INTERVAL, ChangeStateX, TIME_MICROS);
+  taskManager.scheduleFixedRate(SPEED_INTERVAL, ChangeStateZ, TIME_MICROS);
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
